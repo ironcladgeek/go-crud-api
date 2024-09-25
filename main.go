2b19a8575bd5ce1ccfe7a20bc0ecc6331c47d1d9
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2024-09-25 18:39:14 +0000

    feat: Add endpoint to create users

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/kataras/iris/v12"
 )
 
@@ -9,6 +11,21 @@ func main() {
 
 	app.Get("/", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{"message": "Hello World!"})
+	})
+
+	app.Post("/users", func(ctx iris.Context) {
+		var user User
+		if err := ctx.ReadJSON(&user); err != nil {
+			ctx.StatusCode(iris.StatusBadRequest)
+			ctx.JSON(iris.Map{"error": err.Error()})
+			return
+		}
+
+		// Print the user data to the console
+		fmt.Printf("Received user: %+v\n", user)
+
+		// Return a success message
+		ctx.JSON(iris.Map{"message": "User created successfully", "user": user})
 	})
 
 	app.Listen(":8080")
